3e68b679440e86a32bdd95c7866c653780a245af
liuwang <liuwang@ilarge.cn>
2020-01-03 10:18:54 +0800

    Question 4, 5 Not Resolved

---
+++ src/twenty/jan/three/question4/Solution.java
@@ -0,0 +1,45 @@
+package twenty.jan.three.question4;
+
+/**
+ * @author liuwang
+ * @date 2020/1/3 9:16
+ *
+ * There are two sorted arrays nums1 and nums2 of size m and n respectively.
+ *
+ * Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+ *
+ * You may assume nums1 and nums2 cannot be both empty.
+ *
+ * Example 1:
+ *
+ * nums1 = [1, 3]
+ * nums2 = [2]
+ *
+ * The median is 2.0
+ * Example 2:
+ *
+ * nums1 = [1, 2]
+ * nums2 = [3, 4]
+ *
+ * The median is (2 + 3)/2 = 2.5
+ *
+ * 给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
+ *
+ * 请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
+ *
+ * 你可以假设 nums1 和 nums2 不会同时为空。
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/median-of-two-sorted-arrays
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+
+public class Solution {
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        return 0.0;
+    }
+
+}
---
+++ src/twenty/jan/three/question5/Solution.java
@@ -0,0 +1,32 @@
+package twenty.jan.three.question5;
+
+/**
+ * @author liuwang
+ * @date 2020/1/3 10:06
+ *
+ * Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+ *
+ * Example 1:
+ *
+ * Input: "babad"
+ * Output: "bab"
+ * Note: "aba" is also a valid answer.
+ * Example 2:
+ *
+ * Input: "cbbd"
+ * Output: "bb"
+ *
+ * 给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/longest-palindromic-substring
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+public class Solution {
+
+    public String longestPalindrome(String s) {
+        return "";
+    }
+
+}
